55cce8e68d27ef78dfcc5f306ae04ade27e219e0
archis <vazearchis@gmail.com>
2022-12-10 09:22:34 +0530

    switched to render.com

--- src/socketConfig.js
+++ src/socketConfig.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io("https://tic-tac-toe-archis.herokuapp.com/")
+const socket = io("https://tic-tac-toe-server.onrender.com/")
 
 export default socket;
